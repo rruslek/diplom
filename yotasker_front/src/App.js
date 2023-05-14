8cee8ca8a78efdb08aecc2453204c7e6990b9d06
rruslek <rouslan154@gmail.com>
2023-05-15 04:37:46 +0500

    fixed

---
+++ yotasker_front/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import { Auth } from './pages/Auth'
+import { Main } from './pages/Main'
+import { Route, Routes } from 'react-router';
+
+const App = () => {
+  return (
+      <Auth/>
+  )
+}
+
+export default App;
